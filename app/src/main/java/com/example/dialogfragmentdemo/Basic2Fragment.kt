760c1f51d05345983a058042c5b7ad33296e3637
Rick <givemepassxd999@gmail.com>
2019-12-17 22:14:36 +0800

    first commit

--- app/src/main/java/com/example/dialogfragmentdemo/Basic2Fragment.kt
+++ app/src/main/java/com/example/dialogfragmentdemo/Basic2Fragment.kt
@@ -5,18 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.dialog_content.view.*
 
-class BasicFragment: Fragment() {
+class Basic2Fragment: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val v = LayoutInflater.from(context).inflate(R.layout.dialog_content, container, false)
-        v.text_view.setOnClickListener {
-            TestDialogFragment().show(childFragmentManager, "")
-        }
-        return v
+        return LayoutInflater.from(context).inflate(R.layout.dialog_content_2, container, false)
     }
 }
--- app/src/main/java/com/example/dialogfragmentdemo/BasicFragment2.kt
+++ app/src/main/java/com/example/dialogfragmentdemo/BasicFragment2.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.dialog_content.view.*
 
-class BasicFragment: Fragment() {
+class BasicFragment2: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,7 +15,14 @@ class BasicFragment: Fragment() {
     ): View? {
         val v = LayoutInflater.from(context).inflate(R.layout.dialog_content, container, false)
         v.text_view.setOnClickListener {
-            TestDialogFragment().show(childFragmentManager, "")
+            TestDialog(it.context).show()
+//            TestDialogFragment2().show(childFragmentManager, "")
+//            AlertDialog.Builder(context)
+//                .setTitle("aaa")
+//                .setMessage("bbb")
+//                .setPositiveButton("ok") { _, _ ->
+//                }
+//                .show()
         }
         return v
     }
